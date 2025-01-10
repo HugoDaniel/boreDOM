3c2963016de2a0a418bffc25829a696d76046b66
HugoDaniel <mail@hugodaniel.pt>
2025-01-10 23:32:23 +0000

    Include dist

--- dist/boreDOM.d.ts
+++ dist/boreDOM.d.ts
@@ -3,11 +3,13 @@
 export type WebComponentDetail = {
 	index: number;
 	name: string;
+	data?: any;
 };
 export type WebComponentInitParams<S> = {
 	detail: WebComponentDetail;
 	state: S;
 	refs: Refs;
+	self: Bored;
 	on: (eventName: string, eventHandler: () => void) => void;
 };
 export type WebComponentRenderParams<S> = {
@@ -15,13 +17,17 @@ export type WebComponentRenderParams<S> = {
 	state: S;
 	refs: Refs;
 	slots: Slots;
+	self: Bored;
+	makeComponent: (tag: string, options?: {
+		detail?: WebComponentDetail;
+	}) => Bored;
 };
 /** The function returned by `webComponent`, used to create subscribers and call the initialize function */
 export type LoadedFunction = ReturnType<typeof webComponent>;
 /** The function passed as parameter to `webComponent`, used to initialize the component and create the render function */
 export type InitFunction<S> = (options: WebComponentInitParams<S>) => RenderFunction<S>;
 /** The function used to render function and update it visually */
-export type RenderFunction<S> = (renderOpts: WebComponentRenderParams<S>, c?: Bored) => void;
+export type RenderFunction<S> = (renderOpts: WebComponentRenderParams<S>) => void;
 export type AppState<S> = {
 	app: S | undefined;
 	internal: {
@@ -46,9 +52,15 @@ declare abstract class Bored extends HTMLElement {
 	abstract renderCallback: (elem: Bored) => void;
 }
 /**
- * Initializer function, it queries all `<template>` elements that
+ * Queries all `<template>` elements that
  * have a `data-component` attribute defined and creates web components
  * with the tag name in that attribute.
+ *
+ * @param state An optional initial app state object. When provided this will
+ * be proxified to allow for automatic updates of the dom whenever it
+ * changes.
+ *
+ * @returns The app initial state.
  */
 export declare function inflictBoreDOM<S extends object>(state?: S): Promise<S | undefined>;
 /**
@@ -56,7 +68,6 @@ export declare function inflictBoreDOM<S extends object>(state?: S): Promise<S |
  *
  * @param initFunction Initialization function that returns the render function
  * @return A curried function to use as callback for component initialization
- * (useful to pass as the 2nd param of `createComponent()`)
  */
 export declare function webComponent<S>(initFunction: InitFunction<S | undefined>): (appState: AppState<S>, detail?: any) => (c: Bored) => void;
 
